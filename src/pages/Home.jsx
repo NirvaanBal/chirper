680b7461f021892dbdf351abe45adeb90d7ed81c
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-05-22 17:24:56 +0530

    Create home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+
+function SignIn() {
+  return (
+    <div className="home">
+      <header>
+        <h1>Happening now</h1>
+        <h2>Join Chirper today.</h2>
+      </header>
+
+      <main>
+        <section className="sign-up">{/* Sign Up */}</section>
+
+        <section className="sign-in">
+          <p className="sign-in-label">Already have an account?</p>
+          <Link className="sign-in-btn" to="/sign-in">
+            Sign In
+          </Link>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default SignIn;
