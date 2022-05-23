6b9bf17f9cc7458dbdf930c952479aa721309fe4
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-05-23 18:06:17 +0530

    Create tweet generator form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from 'react';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import {
+  getAuth,
+  onAuthStateChanged,
+  reauthenticateWithCredential,
+} from 'firebase/auth';
 import { app } from '../firebase.config';
 import { useNavigate } from 'react-router-dom';
 
 function Home() {
   const [user, setUser] = useState({});
+  const [tweet, setTweet] = useState('');
+  const [rows, setRows] = useState(1);
   const [loading, setLoading] = useState(true);
 
   const navigate = useNavigate();
@@ -22,6 +28,15 @@ function Home() {
     });
   }, []);
 
+  const onTweetChange = (e) => {
+    setTweet(e.target.value);
+  };
+
+  const submitTweet = (e) => {
+    e.preventDefault();
+    console.log(user.uid);
+  };
+
   if (loading) return <h2>Loading...</h2>;
 
   return (
@@ -33,8 +48,34 @@ function Home() {
         </button>
       </section>
       <section className="user-home">
-        <h1>Home</h1>
-        <p>lorem10000</p>
+        <h1 style={{ fontSize: '1.6rem', marginBottom: '20px' }}>Home</h1>
+
+        <form onSubmit={submitTweet}>
+          <div className="text-field">
+            <textarea
+              type="text"
+              name="tweet"
+              id="tweet"
+              placeholder="What's happening?"
+              required
+              value={tweet}
+              onChange={onTweetChange}
+              rows={rows}
+            />
+          </div>
+          <div className="text-info">
+            <div className="characters">
+              {tweet.length <= 140 ? (
+                <p>{140 - tweet.length} characters left</p>
+              ) : (
+                <p style={{ color: 'indianred' }}>{140 - tweet.length}</p>
+              )}
+            </div>
+            <button type="submit" disabled={tweet.length < 1 ? true : false}>
+              Tweet
+            </button>
+          </div>
+        </form>
       </section>
       <section className="search">Search...</section>
     </main>
