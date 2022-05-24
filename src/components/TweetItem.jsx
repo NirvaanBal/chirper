35865e023a31d3bdf4a102c6b579435f305c610a
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-05-24 10:45:03 +0530

    Create tweet item component

---
+++ src/components/TweetItem.jsx
@@ -0,0 +1,27 @@
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/Ai';
+
+function TweetItem({ tweet: { likes, timestamp, tweet, userid } }) {
+  const postDate = () => {
+    const date = new Date(timestamp.seconds * 1000).toDateString();
+    return date;
+  };
+
+  return (
+    <div className="tweet-item">
+      <div className="user-and-date">
+        <h4 className="user">later...</h4>
+        <p>
+          <small>{postDate()}</small>
+        </p>
+      </div>
+      <p>{tweet}</p>
+      <div className="tweet-item-actions">
+        <button className="likes">
+          {likes < 1 ? <AiOutlineHeart /> : <AiFillHeart />} {likes}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default TweetItem;
