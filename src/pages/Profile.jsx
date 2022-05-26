79152c133695f3e042f59fd335dee6eaf6b623e8
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-05-26 09:51:13 +0530

    Create profile component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,42 @@
+import { useState, useEffect } from 'react';
+import { Navigate, useNavigate, useParams, Link } from 'react-router-dom';
+import { getDocs, doc, collection, query, where } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { app, db } from '../firebase.config';
+
 function Profile() {
-  return <div>PROFILE</div>;
+  const [user, setUser] = useState({});
+
+  const navigate = useNavigate();
+  const params = useParams();
+
+  useEffect(() => {
+    const user = getAuth(app);
+    if (!user.currentUser) navigate('/sign-in');
+  }, []);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const q = query(
+        collection(db, 'users'),
+        where('userid', '==', params.userId)
+      );
+      const qSnapshot = await getDocs(q);
+
+      qSnapshot.forEach((doc) => setUser(doc.data()));
+    };
+
+    getUser();
+  }, []);
+
+  return (
+    <div className="user-profile">
+      <main>
+        <Link to="/home">Home</Link>
+        <h2>Coming soon</h2>
+      </main>
+    </div>
+  );
 }
 
 export default Profile;
